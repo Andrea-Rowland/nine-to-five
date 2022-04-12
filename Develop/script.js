4fc5940ddb58d291988f0d77cc6055c84da5ff82
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-04-11 19:03:04 -0600

    added time array

---
+++ Develop/script.js
@@ -0,0 +1,21 @@
+console.log("test");
+var d = new Date()
+var container = $(".container");
+var timeArray = [
+    "9AM",
+    "10AM",
+    "11AM",
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",    
+]
+
+console.log(d.getHours())
+// for loop out of time array
+for (let i = 0; i < timeArray.length; i++) {
+
+}
+
+//create elements using jquery, then append the elements to the div container
