4b6fc5b208585362fbcb7762f87d993d73f5dad2
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-04-13 20:57:40 -0600

    added more HTML

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,25 @@
-console.log("test");
+// GLOBAL VARIABLES
 var d = new Date()
 var container = $(".container");
+var time = [9,10, 11, 12, 1, 2, 3, 4, 5]
+
+// MEDIA QUERIES
+var currentDay = moment().format("MMMM Do YYYY");
+
+$("#currentDay").text(moment().format("MMMM Do YYYY"));
+// END MEDIA QUERIES
+
+console.log("test");
+
+// time array
+
+for (const timeSetep of time) {
+    var dateInput = $("<input>")
+    .addClass("row")
+    
+}
+
+$(".container")
 var timeArray = [
     "9AM",
     "10AM",
@@ -19,3 +38,11 @@ for (let i = 0; i < timeArray.length; i++) {
 }
 
 //create elements using jquery, then append the elements to the div container
+
+
+
+
+
+
+
+
